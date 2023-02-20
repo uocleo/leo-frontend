a9fcaaf58512a12f44812bcdf3a59353df33f437
Thevindu <kulasinghet@gmail.com>
2023-02-20 20:32:44 +0530

    add loader

--- js/index.js
+++ js/index.js
@@ -59,3 +59,17 @@ $(".hover").mouseleave(
 		$(this).removeClass("hover");
 	}
 );
+
+
+$(document).ready(function(){
+	$("#testimonial-slider").owlCarousel({
+		items:1,
+		itemsDesktop:[1000,1],
+		itemsDesktopSmall:[979,1],
+		itemsTablet:[768,1],
+		pagination:false,
+		navigation:true,
+		navigationText:["",""],
+		autoPlay:true
+	});
+});
