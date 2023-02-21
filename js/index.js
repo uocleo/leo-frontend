70374bf51fb0f4de431dc0d54c65f94066929469
Thevindu <kulasinghet@gmail.com>
2023-02-21 09:16:35 +0530

    add movement section

--- js/index.js
+++ js/index.js
@@ -34,15 +34,15 @@ $(window).scroll(function () {
 
 
 			});
-		} , 1000);
+		}, 1000);
 
 	}
 
 });
 
-// if scrolled more than 100px from the top then add class "bg-dark" to the header remove "bg-dark" when not
+// if scrolled more than 100vh from the top then add class "bg-dark" to the header remove "bg-dark" when not
 $(window).scroll(function () {
-	if ($(this).scrollTop() > 920) {
+	if ($(this).scrollTop() > 500) {
 		$('header').addClass('bg-dark');
 		$('header').removeClass('bg-light');
 		document.getElementById("logo").src = "./public/logo-colour.png";
@@ -61,17 +61,13 @@ $(".hover").mouseleave(
 );
 
 
-
-
-
-$(document).ready(function() {
-	$(".loader").delay(5000).fadeOut("slow");
-	$(".loader-wrapper").delay(5000).fadeOut("slow");
+$(document).ready(function () {
+	$(".loader").delay(10).fadeOut("slow");
+	$(".loader-wrapper").delay(10).fadeOut("slow");
 });
 
 
-
-setInterval(function() {
+setInterval(function () {
 	var currentSlide = document.querySelector('.slider__nav:checked');
 	var nextSlide = currentSlide.nextElementSibling || document.querySelector('.slider__nav');
 	nextSlide.checked = true;
@@ -86,4 +82,73 @@ document.addEventListener('DOMContentLoaded', () => {
 			behavior: 'smooth'
 		});
 	});
+});
+
+
+$(document).ready(function () {
+	const testimonials = [
+		{
+			name: "Eva Sawyer",
+			job: "CEO, Fashworks",
+			image: "https://i.postimg.cc/mgp4pfz5/profile-image-1.png",
+			testimonial:
+				"Neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur",
+		},
+		{
+			name: "Katey Topaz",
+			job: "Developer, TechCrew",
+			image: "https://i.postimg.cc/PfSSwtB9/profile-image-2.png",
+			testimonial:
+				"Elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla",
+		},
+		{
+			name: "Jae Robin",
+			job: "UI Designer, Affinity Agency",
+			image: "https://i.postimg.cc/W4mnbjG9/profile-image-3.png",
+			testimonial:
+				"Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis",
+		},
+		{
+			name: "Nicola Blakely",
+			job: "CEO,Zeal Wheels",
+			image: "https://i.postimg.cc/xdLsJL23/profile-image-4.png",
+			testimonial:
+				"Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
+		},
+	];
+
+//Current Slide
+	let i = 0;
+//Total Slides
+	let j = testimonials.length;
+
+	let testimonialContainer = document.getElementById("testimonial-container");
+	let nextBtn = document.getElementById("next");
+	let prevBtn = document.getElementById("prev");
+
+	nextBtn.addEventListener("click", () => {
+		i = (j + i + 1) % j;
+		displayTestimonial();
+	});
+	prevBtn.addEventListener("click", () => {
+		i = (j + i - 1) % j;
+		displayTestimonial();
+	});
+
+	let displayTestimonial = () => {
+		testimonialContainer.innerHTML = `
+    <p>${testimonials[i].testimonial}</p>
+    <img src=${testimonials[i].image}>
+    <h3>${testimonials[i].name}</h3>
+    <h6>${testimonials[i].job}</h6>
+  `;
+	};
+
+	// add set interval here to change the testimonial every 5 seconds
+	setInterval(function () {
+		i = (j + i + 1) % j;
+		displayTestimonial();
+	}, 5000);
+
+	window.onload = displayTestimonial;
 });
