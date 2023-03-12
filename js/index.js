110d3b3db84cbf03c4c532fd8186650bfb0cce72
Thevindu Kulasinghe <kulasinghet@gmail.com>
2023-03-12 15:30:42 +0530

    add modal for project details

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
-// run below code if scroll to the element counter-stats
+const projects = [
+	{
+		id: 1,
+		title: 'Survival 5.0',
+		category: 'Community Service',
+		chair: 'Theruwandi Edirisinghe',
+		description: 'The project "Survival 5.0" was implemented by the Leo Club of University of Colombo for the fifth consecutive year with the aim of raising community awareness and implementing prevention programs to reduce the impact of natural or man-made disasters in Sri Lanka. The phase one was carried out on 8th January 2023 at Kegalle Aranayaka Manadi Primary School. Students and their parents from Manadi Primary School\'s Grades 4 and 5 attended the awareness program, which was followed by a practical training activity program led by officials from the Kegalle District Disaster Management Center.  The second phase was implemented on 3rd of February 2023 at Rathnapura East Mudunkotuwa Vidyalaya. The awareness session on managing floods was conducted by Mr. S.H.M. Manjula, Assistant Director, Disaster Management Center, Rathnapura. While the awareness session was in progress the interlock stones donation was carried out. From this, it is expected to redevelop the damaged area near the school main hall due to flooding and facilitate the entire school community in carrying out their daily operations without any hassle arising from heavy rain and flooding. In conclusion, team Survival 5.0 was very satisfied with what was done in two phases in Rathnapura and Kegalle to help our own community in reducing impacts and overcoming natural disasters. The benefit created for the community is long-lasting since the students or children who participated in the awareness sessions have the strong power of spreading the message of managing disasters to society. '
+	},
+	{
+		id: 2,
+		title: 'UOC Leos Get-Together 23',
+		category: 'Community Service',
+		chair: 'Leo Club of University of Colombo',
+		description: 'The project "Survival 5.0" was implemented by the Leo Club of University of Colombo for the fifth consecutive year with the aim of raising community awareness and implementing prevention programs to reduce the impact of natural or man-made disasters in Sri Lanka. The phase one was carried out on 8th January 2023 at Kegalle Aranayaka Manadi Primary School. Students and their parents from Manadi Primary School\'s Grades 4 and 5 attended the awareness program, which was followed by a practical training activity program led by officials from the Kegalle District Disaster Management Center.  The second phase was implemented on 3rd of February 2023 at Rathnapura East Mudunkotuwa Vidyalaya. The awareness session on managing floods was conducted by Mr. S.H.M. Manjula, Assistant Director, Disaster Management Center, Rathnapura. While the awareness session was in progress the interlock stones donation was carried out. From this, it is expected to redevelop the damaged area near the school main hall due to flooding and facilitate the entire school community in carrying out their daily operations without any hassle arising from heavy rain and flooding. In conclusion, team Survival 5.0 was very satisfied with what was done in two phases in Rathnapura and Kegalle to help our own community in reducing impacts and overcoming natural disasters. The benefit created for the community is long-lasting since the students or children who participated in the awareness sessions have the strong power of spreading the message of managing disasters to society.'
+	}
+];
+
 
 $(window).scroll(function () {
 	var hT = $('#counter-stats').offset().top,
@@ -320,3 +336,91 @@ $(document).ready(function () {
 	});
 
 });
+
+
+function getProjectDetails(projectName) {
+
+	const project = projects.find(project => project.title === projectName);
+	return project;
+}
+
+
+function showDetailsOnClickProject(projectName) {
+
+	const project = getProjectDetails(projectName);
+	const projectDetails = document.getElementById('project-details');
+
+
+	// create boostrap model
+	const modal = document.createElement('div');
+	modal.classList.add('modal', 'fade');
+	modal.id = 'projectModal';
+	modal.setAttribute('tabindex', '-1');
+	modal.setAttribute('aria-labelledby', 'projectModalLabel');
+	modal.setAttribute('aria-hidden', 'true');
+
+const modalDialog = document.createElement('div');
+	modalDialog.classList.add('modal-dialog');
+	modalDialog.classList.add('modal-dialog-centered');
+	modalDialog.classList.add('modal-lg');
+
+const modalContent = document.createElement('div');
+	modalContent.classList.add('modal-content');
+
+const modalHeader = document.createElement('div');
+	modalHeader.classList.add('modal-header');
+
+const modalTitle = document.createElement('h5');
+	modalTitle.classList.add('modal-title');
+
+const modalCloseButton = document.createElement('button');
+	modalCloseButton.classList.add('btn-close');
+	// data-bs-dismiss="modal"
+	modalCloseButton.setAttribute('data-bs-dismiss', 'modal');
+
+const modalBody = document.createElement('div');
+	modalBody.classList.add('modal-body');
+
+
+	// <div>
+	// 	<h1>Project Chair: </h1> + project.chair
+	// 	<h2>Category: </h2>
+	// </div>
+
+	// append elements to modal
+	modalTitle.appendChild(document.createTextNode(project.title));
+	modalHeader.appendChild(modalTitle);
+	modalHeader.appendChild(modalCloseButton);
+
+	// add projeact chair as h1
+	const projectChair = document.createElement('h1');
+	projectChair.textContent = "Project Chair: " + project.chair;
+	modalBody.appendChild(projectChair);
+
+	const projectCategory = document.createElement('h2');
+	projectCategory.textContent = "Category: " + project.category;
+	modalBody.appendChild(projectCategory);
+
+	modalBody.appendChild(document.createElement('br'));
+	modalBody.appendChild(document.createTextNode("Description: " + project.description));
+	modalContent.appendChild(modalHeader);
+	modalContent.appendChild(modalBody);
+	modalDialog.appendChild(modalContent);
+	modal.appendChild(modalDialog);
+
+
+	// append modal to project details
+	projectDetails.appendChild(modal);
+
+
+	// show modal
+	const projectModal = new bootstrap.Modal(document.getElementById('projectModal'));
+	projectModal.show();
+
+
+}
+
+function closeProjectDetails() {
+	const projectDetails = document.getElementById('project-details');
+	projectDetails.style.display = 'none';
+}
